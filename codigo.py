9956ba293a38fcd89eb9678ec17fea88d390dcae
Julio Chilela <julio.chilela@angolacables.co.ao>
2022-12-12 08:48:17 +0100

    spark

--- codigo.py
+++ codigo.py
@@ -1,9 +1,5 @@
 import pyspark
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType,StructField, StringType, IntegerType 
-from pyspark.sql.types import ArrayType, DoubleType, BooleanType
-from pyspark.sql.functions import col,array_contains
-
 spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
 
 df = spark.read.csv("/home/ubuntu/dados/Data7602DescendingYearOrder.csv")
